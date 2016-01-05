84a897ac919e7d5eb51406776f36c71eff5beb15
whitehaven <accrist@gmail.com>
2016-01-05 01:59:52 -0700

    first real work going, first type, Producer, is made

--- Undulation-Devastation.py
+++ Undulation-Devastation.py
@@ -1,19 +1,32 @@
-import cocos
+import pyglet
+
+import primitives
+
+window = pyglet.window.Window()
+counter = 0
 
 
-class SimulationWindow(cocos.layer.Layer):
-    def __init__(self):
-        super(SimulationWindow, self).__init__()
-
-        label = cocos.text.Label('Hello, world',
-                                 font_name='Times New Roman',
-                                 font_size=32,
-                                 anchor_x='center', anchor_y='center')
-        label.position = 320, 240
-        self.add(label)
+class Producer(object):
+    def __init__(self, x, y, width, health, color=(0, 0, 0, 1)):
+        self.shape = primitives.Circle(x, y, z=0, width=2, color=color, stroke=width / 2, rotation=0.0,
+                                       style=pyglet.gl.GLU_FILL)
+        self.health = health
 
 
-cocos.director.director.init()
-sim_layer = SimulationWindow()
-main_scene = cocos.scene.Scene(sim_layer)
-cocos.director.director.run(main_scene)
+# class Collision? with collided members
+
+def update_frame(dt):
+    global counter
+    counter += 1
+
+
+@window.event
+def on_draw():
+    print(counter)
+    pyglet.gl.glClearColor(0, 0, 0, 0)
+    window.clear()
+
+
+pyglet.clock.schedule_interval(update_frame, 1 / 10.0)
+
+pyglet.app.run()
